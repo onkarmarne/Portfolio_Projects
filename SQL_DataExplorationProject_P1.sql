56b0ec777ad133ff73b5d63c6c0b855356d094cb
onkarmarne <83461884+onkarmarne@users.noreply.github.com>
2024-06-20 13:20:37 +0530

    Update SQL_DataExplorationProject_P1.sql

--- SQL_DataExplorationProject_P1.sql
+++ SQL_DataExplorationProject_P1.sql
@@ -6,15 +6,13 @@ select location, date, total_cases, new_cases, total_deaths, population
 from Portfolio_Project.dbo.Covid_Deaths
 order by 1,2
 
-
+ 
 /* Looking at Total Cases Vs. Total Deaths, likelihood of dying if you contract covid in your country */
 
 Select location, date, total_cases,total_deaths, (cast (total_deaths as float)/ (total_cases as float))*100 as Death_Percentage
 From Portfolio_Project..Covid_Deaths
 Where location like '%ndia'
 order by 1,2
-
-
 
 
 /* Looking at total cases Vs. Population, shows what percentage of population got covid */
@@ -24,7 +22,9 @@ from Covid_Deaths
 where location like '%ndia'
 order by 1,2
 
-/* Looking at countries with highest infection rate compared to population */
+
+/* Looking at countries with highest infection rate as compared to population */
+
 select location, population, MAX(total_cases) AS MAX_CASES, MAX((total_cases/population))*100 AS Percentage_Of_Infection
 from Covid_Deaths
 group by location, population
@@ -32,6 +32,7 @@ order by Percentage_Of_Infection desc
 
 
 /* showing countries with highest death count per population */
+
 select location, MAX(cast(total_deaths as int)) as total_deaths_per_population
 from Covid_Deaths
 where continent is not null
@@ -40,12 +41,15 @@ order by total_deaths_per_population desc
 
 
 /* breaking down things by continent OR showing continents with highest death count per population */
+
 select continent, MAX(cast(total_deaths as int)) as total_deaths_per_population
 from Covid_Deaths
 group by continent
 order by total_deaths_per_population desc
 
+
 /* Global Numbers */
+
 select * from Covid_Deaths
 
 select date, sum(new_cases) as total_new_cases, sum(cast(new_deaths as int)) as total_new_deaths
@@ -55,12 +59,12 @@ where continent is not null
 group by date
 order by 1,2
 
-
 select sum(new_cases) as total_new_cases, sum(cast(new_deaths as int)) as total_new_deaths, 
 sum(cast(new_deaths as int))/sum(new_cases)*100 as death_percentage
 from Covid_Deaths
 where continent is not null
 order by 1,2
+
 
 /* Looking at Total Population Vs. Vaccinations */
 -- 1) using CTE and 
@@ -102,8 +106,8 @@ create table #Percentage_of_Population_Vaccinated
 (continent nvarchar(255), location nvarchar(255), date datetime, 
  population numeric, new_vaccinations numeric, Vaccinations_count numeric,)
  
- Insert into #Percentage_of_Population_Vaccinated
- select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+Insert into #Percentage_of_Population_Vaccinated
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(Convert(bigint, vac.new_vaccinations)) over (partition by dea.location order by dea.location, dea.date) as Vaccinations_count
 --, (Vaccinations_count/population)*100
 from Covid_Deaths as dea Join Covid_Vaccinations as vac
@@ -113,15 +117,17 @@ where dea.continent is not null
 
 select *, (Vaccinations_count/population)*100 as Vaccinated_Population_Percentage from #Percentage_of_Population_Vaccinated
 
+
 /* creating view for storing data for visualizations in tableau */
 
 create view Percentage_of_Population_Vaccinated as
- select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(Convert(bigint, vac.new_vaccinations)) over (partition by dea.location order by dea.location, dea.date) as Vaccinations_count
 --, (Vaccinations_count/population)*100
 from Covid_Deaths as dea Join Covid_Vaccinations as vac
 on dea.location = vac.location and dea.date = vac.date
 where dea.continent is not null
 -- order by 2,3
+
 
 select * from Percentage_of_Population_Vaccinated --View created and its usage 
